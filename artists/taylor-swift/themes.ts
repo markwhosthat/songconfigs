9d4c1238d384a3b2e4261fa552945e4f6a31ca65
mark <47726412+markwhosthat@users.noreply.github.com>
2026-03-02 13:14:20 +1100

    Add themes for Taylor Swift albums

---
+++ artists/taylor-swift/themes.ts
@@ -0,0 +1,45 @@
+import type { Theme } from '../../core/types';
+
+export const DEFAULT_THEME: Theme = {
+    className: 'theme-default',
+};
+
+export const ALBUM_THEMES: Record<string, Theme> = {
+    "Taylor Swift": {
+        className: 'theme-taylor-swift',
+    },
+    "Fearless": {
+        className: 'theme-fearless',
+    },
+    "Speak Now": {
+        className: 'theme-speak-now',
+    },
+    "Red": {
+        className: 'theme-red',
+    },
+    "1989": {
+        className: 'theme-1989',
+    },
+    "Reputation": {
+        className: 'theme-reputation',
+    },
+    "Lover": {
+        className: 'theme-lover',
+    },
+    "Folklore": {
+        className: 'theme-folklore',
+    },
+    "Evermore": {
+        className: 'theme-evermore',
+    },
+    "Midnights": {
+        className: 'theme-midnights',
+    },
+    "The Tortured Poets Department": {
+        className: 'theme-ttpd',
+        short_name: "TTPD",
+    },
+    "The Life Of A Showgirl": {
+        className: 'theme-showgirl',
+    },
+}
